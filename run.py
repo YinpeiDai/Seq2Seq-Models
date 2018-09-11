842791f4b51226de8100881f0f09c566d5c1c1e1
Y Dai (visitor - Gasic) <yd300@taipei>
2018-09-11 17:35:15 +0100

    fix bug

--- run.py
+++ run.py
@@ -8,26 +8,22 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from train_manager import Train_BasicEncoderDecoder,Train_AttenNet, Train_CopyNet
 
-# # parse the cmd arguments
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--model", type=str,
-#                     choices=[
-#                         'BasicSeq2Seq', 'AttenNet', 'CopyNet'
-#                     ])
-# args = parser.parse_args()
-# model = args.model
+# parse the cmd arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str,
+                    choices=[
+                        'BasicSeq2Seq', 'AttenNet', 'CopyNet'
+                    ])
+args = parser.parse_args()
+model = args.model
 
 
-# if model == 'BasicSeq2Seq':
-#     manager = Train_BasicEncoderDecoder()
-#     manager.run_model()
-# elif model == 'AttenNet':
-#     manager = Train_AttenNet()
-#     manager.run_model()
-# else:
-#     manager = Train_CopyNet()
-#     manager.run_model()
-
-if __name__ == '__main__':
+if model == 'BasicSeq2Seq':
+    manager = Train_BasicEncoderDecoder()
+    manager.run_model()
+elif model == 'AttenNet':
+    manager = Train_AttenNet()
+    manager.run_model()
+else:
     manager = Train_CopyNet()
     manager.run_model()
